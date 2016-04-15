d0e3bea19a3cb3cd10662a60278af4d381b2d0a1
joneqian <qian.qing@aliyun.com>
2016-04-15 09:07:58 +0800

    fix bug

--- server/cloud-soap-interface/object/supplierObj.js
+++ server/cloud-soap-interface/object/supplierObj.js
@@ -7,6 +7,97 @@ var xml = require('xml');
 var utils = require('../../util/utils');
 
 exports.setShoppingImgXML = function (obj) {
+	var imgObj = [];
+	imgObj.push({
+		_attr: {
+			'xmlns:d4p1': 'http://schemas.datacontract.org/2004/07/MYun.BPC.Contract.ShoppingMgmt.Data',
+			'xmlns:i': 'http://www.w3.org/2001/XMLSchema-instance'
+		}
+	});
+	var img = null;
+	var time = utils.formatByT(new Date());
+	for (var i = 0; i < obj.imgUrl.length; i++) {
+		img = {
+			'd4p1:ShoppingImg': [
+				{
+					EditDate: [
+						{
+							_attr: {
+								'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service'
+							}
+						}, time
+					]
+				},
+				{
+					EditUser: [
+						{
+							_attr: {
+								'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service',
+								'i:nil': 'true'
+							}
+						}
+					]
+				},
+				{
+					EditUserSysno: [
+						{
+							_attr: {
+								'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service'
+							}
+						}, 0
+					]
+				},
+				{
+					InDate: [
+						{
+							_attr: {
+								'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service'
+							}
+						}, utils.formatByT(new Date())
+					]
+				},
+				{
+					InUser: [
+						{
+							_attr: {
+								'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service',
+								'i:nil': 'true'
+							}
+						}
+					]
+				},
+				{
+					InUserSysno: [
+						{
+							_attr: {
+								'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service'
+							}
+						}, 0
+					]
+				},
+				{
+					Status: [
+						{
+							_attr: {
+								'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service',
+								'i:nil': 'true'
+							}
+						}
+					]
+				},
+				{
+					'd4p1:ImgKey': obj.orderId
+				},
+				{
+					'd4p1:ImgType': obj.imgType
+				},
+				{
+					'd4p1:ImgValue': obj.imgUrl[i]
+				}
+			]
+		};
+		imgObj.push(img);
+	}
 	var xmlObj = [{
 		SetShoppingImg: [
 			{
@@ -15,7 +106,7 @@ exports.setShoppingImgXML = function (obj) {
 				}
 			},
 			{
-				message:  [
+				message: [
 					{
 						_attr: {
 							'xmlns:d4p1': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service',
@@ -48,93 +139,7 @@ exports.setShoppingImgXML = function (obj) {
 				]
 			},
 			{
-				imgs: [
-					{
-						_attr: {
-							'xmlns:d4p1': 'http://schemas.datacontract.org/2004/07/MYun.BPC.Contract.ShoppingMgmt.Data',
-							'xmlns:i': 'http://www.w3.org/2001/XMLSchema-instance'
-						}
-					},
-					{
-						'd4p1:ShoppingImg': [
-							{
-								EditDate: [
-									{
-										_attr: {
-											'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service'
-										}
-									}, utils.formatByT(new Date())
-								]
-							},
-							{
-								EditUser: [
-									{
-										_attr: {
-											'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service',
-											'i:nil': 'true'
-										}
-									}
-								]
-							},
-							{
-								EditUserSysno: [
-									{
-										_attr: {
-											'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service'
-										}
-									}, 0
-								]
-							},
-							{
-								InDate: [
-									{
-										_attr: {
-											'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service'
-										}
-									}, utils.formatByT(new Date())
-								]
-							},
-							{
-								InUser: [
-									{
-										_attr: {
-											'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service',
-											'i:nil': 'true'
-										}
-									}
-								]
-							},
-							{
-								InUserSysno: [
-									{
-										_attr: {
-											'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service'
-										}
-									}, 0
-								]
-							},
-							{
-								Status: [
-									{
-										_attr: {
-											'xmlns': 'http://schemas.datacontract.org/2004/07/MYun.Framework.Service',
-											'i:nil': 'true'
-										}
-									}
-								]
-							},
-							{
-								'd4p1:ImgKey': obj.orderId
-							},
-							{
-								'd4p1:ImgType': obj.imgType
-							},
-							{
-								'd4p1:ImgValue': obj.imgUrl
-							}
-						]
-					}
-				]
+				imgs: imgObj
 			}
 		]
 	}];
